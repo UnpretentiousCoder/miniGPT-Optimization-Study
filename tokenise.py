560cd18e14e0a3eeb25505ad1ced0444191b4ebf
shunyuann <166272522+UnpretentiousCoder@users.noreply.github.com>
2026-06-23 21:28:40 +0800

    added_tokening and train test split

--- tokenise.py
+++ tokenise.py
@@ -1,7 +1,11 @@
 import requests
+import torch
 
 url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
 text = requests.get(url).text
+print(torch.__version__)          # should be 2.0+
+print(torch.cuda.is_available())  # should be True
+print(torch.cuda.get_device_name(0))  # should show RTX 4060
 
 unique_chars = sorted(list(set(text))) #sorting it because we want to have a consistent order of characters
 print(f"Unique characters: {len(unique_chars)}")
@@ -13,6 +17,7 @@ decode = {i: char for i, char in enumerate(unique_chars)}
 encoder = lambda s: [encode[char] for char in s]
 decoder = lambda l: [decode[i] for i in l]
 
-print(f" Encoded text: {encoder('hello world')}")
-print(f"Decoded: {decoder(encoder('hello world'))}")
-print(f"Decoded text: { ''.join(decoder(encoder('hello world')))}")
+encoded_text = encoder(text)
+data = torch.tensor(encoded_text, dtype=torch.long) #stores the encoded text as a tensor of long integers
+print(data.shape, data.dtype) #this is a 1D tensor of shape (1115394,) and dtype torch.int64
+print(data[:500])
---
+++ train.py
@@ -0,0 +1,42 @@
+import requests
+import torch
+
+
+def get_dataset(url):
+    """
+    Fetches the dataset from the given URL and returns the text content.
+    """
+    return requests.get(url).text
+
+def tokenise_by_character(text):
+    """
+    Tokenises the input text into unique characters and provides encoding and decoding functions.
+    """
+    unique_chars = sorted(list(set(text)))
+    encode = {char: i for i, char in enumerate(unique_chars)}
+
+    encoder = lambda s: [encode[char]for char in s]
+
+    encoder_text = encoder(text)
+    return encoder_text, encoder
+
+def train_eval_split(data, train_rate=0.9):
+    """
+    Splits the data into training and evaluation sets based on the specified train_rate.
+    """
+    n = int(len(data) * train_rate)
+    train = data[:n]
+    ev = data[n:]
+    return train, ev
+
+def main():
+    url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+    text = get_dataset(url)
+    
+    print(torch.__version__)          
+    print(torch.cuda.is_available())  
+    print(torch.cuda.get_device_name(0))  
+
+    encoded_text, encoder = tokenise_by_character(text)
+    data = torch.tensor(encoded_text, dtype = torch.long)
+    train_data, eval_data = train_eval_split(data)
