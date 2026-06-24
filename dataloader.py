e1370f778005737deb327c7f282401b51bf8a08e
shunyuann <166272522+UnpretentiousCoder@users.noreply.github.com>
2026-06-24 22:33:29 +0800

    starting on dataloader

--- dataloader.py
+++ dataloader.py
@@ -16,5 +16,16 @@ decoder = lambda l: [decode[i] for i in l]
 
 encoded_text = encoder(text)
 data = torch.tensor(encoded_text, dtype=torch.long) #stores the encoded text as a tensor of long integers
-print(data.shape, data.dtype) #this is a 1D tensor of shape (1115394,) and dtype torch.int64
-print(data[:500])
+
+train_data, eval_data = data[:int(0.9*len(data))], data[int(0.9*len(data)):] #splitting the data into training and evaluation sets
+
+block_size = 8
+train_dataset = train_data[:block_size+ 1]
+
+x = train_data[:block_size]
+y = train_data[1:block_size + 1]
+
+for i in range(block_size):
+    context = x[:i+1]
+    target = y[i]
+    print(f"Context: {context.tolist()} Target: {target}")
