7f0d0fbf2e2e9f8d757cb7073a953012fea355e3
shunyuann <166272522+UnpretentiousCoder@users.noreply.github.com>
2026-07-02 23:07:47 +0800

    loss function and generate

--- bigram_language_model.py
+++ bigram_language_model.py
@@ -9,11 +9,24 @@ class BigramLanguageModel(nn.Module):
     def __init__(self, vocab_size):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size) #this maps each token to a vector of size "vocab_size"
-        #num_embeddings = vocab_size, embedding_dim = vocab_size
+        #num_embeddings = vocab_size, embedding_dim = vocab_size, returns B,T,C (a score for every single character in the vocabulary for every position in the input sequence)
 
     def forward(self, idx, targets):
         logits = self.token_embedding_table(idx) #this returns a tensor of shape (batch_size, block_size, vocab_size)
-        return logits #B,T,C
+        B, T, C = logits.shape
+        logits = logits.view(B*T, C) #converts to a 2D tensor of shape (B*T, C) for the loss function
+        targets = targets.view(B*T)
+        loss = F.cross_entropy(logits, targets)
+        return logits, loss #B,T,C
+    
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx) #runs the self.token_embedding_table
+            logits = logits[:, -1, :] #keep the last time step(column) of the logits, shape (B, C)
+            probs = F.softmax(logits, dim = -1)#converts the logits to probabilities, shape (B, C)
+            idx_next = torch.multinomial(probs, num_samples=1) #returns a tensor of shape (B, 1) with the index of the next token sampled from the probabilities
+            idx = torch.cat((idx, idx_next), dim =1) #this concatenates the new token to the input tensor, shape (B, T+1)
+        return idx
     
 
 url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
@@ -32,5 +45,6 @@ encoded_text = encoder(text)
 data = torch.tensor(encoded_text, dtype=torch.long) #stores the encoded text as a tensor of long integers
 xt, yt = get_batches(split="train")
 m = BigramLanguageModel(vocab_size=65)
-out = m(xt, yt) #runs the forward pass of the model, returns a tensor of shape (batch_size, block_size, vocab_size)
-print(out.shape) #this should print torch.Size([4, 8, 65]) which is (batch_size, block_size, vocab_size)
+logits, loss = m(xt, yt) #runs the forward pass of the model, returns a tensor of shape (batch_size, block_size, vocab_size)
+print(logits.shape) #this should print torch.Size([4, 8, 65]) which is (batch_size, block_size, vocab_size)
+print(loss)
